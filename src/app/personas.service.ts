6b2cd243bdd7ef394b4e79b216189f93312f267c
Federico Aletti <fedeale2000@gmail.com>
2021-12-06 17:28:01 -0300

    Servicio dentro de otro servicio

--- src/app/personas.service.ts
+++ src/app/personas.service.ts
@@ -1,5 +1,8 @@
+import { Injectable } from '@angular/core';
+import { LoggingService } from './LoggingService.service';
 import { Persona } from './persona.model';
 
+@Injectable()
 export class PersonasService {
   personas: Persona[] = [
     new Persona('Juan', 'Perez'),
@@ -8,7 +11,10 @@ export class PersonasService {
     new Persona('Ana', 'Garcia'),
   ];
 
+  constructor(private loggingService: LoggingService) {}
+
   agregarPersona(persona: Persona) {
+    this.loggingService.enviarMensajeAConsola('Se agregó una persona');
     this.personas.push(persona);
   }
 }
