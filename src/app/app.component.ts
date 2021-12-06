2386a7ef095847371486be5ca1aa6b4fc3bce97c
Federico Aletti <fedeale2000@gmail.com>
2021-12-05 21:17:47 -0300

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'listado-personas';
+  titulo = 'Listado de Personas';
+  personas: Persona[] = [
+    new Persona('Juan', 'Perez'),
+    new Persona('Pedro', 'Gomez'),
+    new Persona('Maria', 'Lopez'),
+    new Persona('Ana', 'Garcia'),
+  ];
+
+  nombreInput: string = '';
+  apellidoInput: string = '';
+
+  agregarPersona() {
+    if (this.nombreInput.length > 0 && this.apellidoInput.length > 0) {
+      let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+      this.personas.push(persona1);
+    }
+  }
 }
